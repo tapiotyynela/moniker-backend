65837c85710652f3bf1d551babc477ea23a8438e
tapiotyynela <tapio.tyynela@gmail.com>
2023-03-12 10:07:13 +0200

    initialize web socket

--- src/index.ts
+++ src/index.ts
@@ -10,20 +10,19 @@ import Game from './models/game';
 import Team from './models/team';
 import User from './models/user';
 import Word from './models/word';
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 dotenv.config()
 const app = express()
+// WS connection
 const port = 3000
 
-app.listen(port, () => {
-  console.log('App successfully running in port: ', port)
-})
-
-db.sync({ alter: true })
+db.sync({ alter: true, logging: false })
 initializeDb([Game, Team, User, Word])
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:3001'  
+  origin: ['http://localhost:3001', 'http://localhost:3002']
 }))
 
 app.get('/api/health', (req: Request, res: Response) => {
@@ -33,3 +32,21 @@ app.get('/api/health', (req: Request, res: Response) => {
 app.use('/api/users', userRoutes)
 app.use('/api/word', wordRoutes)
 app.use('/api/game', gameRoutes)
+
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:3001", "http://localhost:3002"]
+  }
+})
+
+io.on("connection", (socket) => {
+  console.log("Websocket initialized.", socket.id)
+  socket.on("USER CONNECTED", (arg) => {
+    console.log(arg)
+  })
+})
+
+httpServer.listen(port, () => {
+  console.log('App successfully running in port: ', port)
+})
